57b452f7b72a03ac731d79e60c182a6b79eed440
Pet Projects <petprojects@Mungo-Digital-Imac1.local>
2018-12-10 07:05:23 -0500

    testing build

--- webpack.config.js
+++ webpack.config.js
@@ -9,17 +9,41 @@
  * 
  */
 
+
+
+ /**
+  * NEED TO LEARN TO DO:
+  * - copy a file from 1 folder to another
+  * - minify HTML, CSS, JS
+  */
+
 const path = require('path');
 const ENV = 'dev'; // 'prod' development or production environments
 const ASSETS_DIR = './src/front-end';
 const BUILD_DIR = `build-${ENV}/front-end`;
 
 module.exports = {
+  // https://webpack.js.org/concepts/mode/
   mode: `${(ENV==='dev'?'development':'prodction')}`,
   entry: `${ASSETS_DIR}/js/test1.js`,
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `${BUILD_DIR}/js/`)
-  }
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: 'css-loader' },
+      { 
+        test: /\.js$/, 
+        exclude: /(node_modules)/, 
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+    ]
+  },
 };
 
