c9500c51e15d110c5dc4794fec9e91bb24d46768
Pet Projects <petprojects@Mungo-Digital-Imac1.local>
2018-12-12 08:24:20 -0500

    Renders CSS bundles

---
+++ src/front-end/js/styles.js
@@ -0,0 +1,7 @@
+/**
+ * The application's stylesheets
+ * ***********************************
+ */
+
+import './../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './../css/test1.css'
